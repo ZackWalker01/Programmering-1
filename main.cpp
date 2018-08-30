853d3d4c6b080cf78e3920fe3bfb9e31a7193aae
ZackWalker01 <zawa1002@live.mark.se>
2018-08-30 13:53:03 +0200

    second commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	                         
+    cout <<"Caesar" <<endl;
+    cout <<"Är" <<endl;
+    cout <<"Kurd";
+    cout << endl;
+    
+	return 0; 
+}
