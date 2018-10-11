c80d40a2e3af1fbca387fdc3924ee6fe00ded297
ZackWalker01 <zawa1002@live.mark.se>
2018-10-11 14:57:42 +0200

    com

---
+++ 2018-10-11/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+{
+// övning 4.2
+//	int tal1;
+// cout << "Skriv antal månader" << endl;
+// cin >> tal1;
+// cout << (tal1/12) << " år och " << (tal1%12) << " månader " << endl;
+
+//int x =4;
+//x = (x+2);
+//cout << x << endl;
+//int i = 0;
+//i = i + 1;
+// i += 1; 
+//  cout << i << endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
